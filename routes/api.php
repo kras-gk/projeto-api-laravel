922e5bd10731d58d6aa066b5f947838689e44593
GustavoBenfica <gustavobmm05@gmail.com>
2024-10-09 21:10:03 -0300

    metodos api

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,12 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/cliente', [ClienteController::class,'listar'] ); 
+Route::get('/cliente/{id}', [ClienteController::class,'listarPeloId'] ); 
+Route::post('/cliente', [ClienteController::class,'salvar'] ); 
+Route::put('/cliente/{id}', [ClienteController::class,'editar'] );
+Route::delete('/cliente/{id}', [ClienteController::class,'deletar'] ); 
+
+
+
+
+
